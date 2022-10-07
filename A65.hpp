79c8265c9612a2f3416ce6ea238ca8587162b76e
Zelin Chen <6692czl@gmail.com>
2022-10-07 12:03:58 -0700

    A65(1)

--- A65.hpp
+++ A65.hpp
@@ -7,5 +7,19 @@ using namespace std;
 // complete two functions
 // swap() ; to swap all three values like (a, b, c) to (c, a, b)
 //              This function will use swaptwo().
-
+void swap(int &a,int &b,int &c)
+{
+    int temp;
+    temp=a;
+    a=b;
+    b=c;
+    c=temp;
+}
 // swaptwo() ; to swap two values (a, b) to (b, a)
+void swaptwo(int &a,int &b)
+{
+    int temp;
+    temp=a;
+    a=b;
+    b=temp;
+}
